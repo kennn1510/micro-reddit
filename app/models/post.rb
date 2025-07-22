3ff204dcde802e6392f8b978fc9494ed66c5acf7
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2025-07-22 16:58:28 -0500

    Refactor Post model to include index and foreign key for user association

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   validates :url, presence: true
   validates :title, presence: true
 
-  belongs_to :user
+  belongs_to :user, index: true, foreign_key: true
   has_many :comments
 end
